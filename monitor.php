314593ab90964a9a29ce2292b09038deba6d4629
friedrichOsDev <f39597054@gmail.com>
2026-03-03 20:39:15 +0100

    add contact, home, monitor, projects pages

---
+++ monitor.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FriedrichOsDev - Monitor</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="header">
+        <p>FriedrichOsDev - Monitor</p>
+        <div>
+            <a class="header-link" href="index.html">Home</a>
+            <a class="header-link" href="projects.html">Projects</a>
+            <a class="header-link header-link-active" href="monitor.php">Monitor</a>
+            <a class="header-link header-link-last" href="contact.html">Contact</a>
+        </div>
+    </header>
+    <main>
+        
+    </main>
+    <footer class="footer">
+        <p>&copy; 2026 FriedrichOsDev. All rights reserved.</p>
+    </footer>
+</body>
+</html>
