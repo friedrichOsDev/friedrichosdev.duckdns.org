87f58102cfd885d0cd7b677249d87fbe372c096b
friedrichOsDev <f39597054@gmail.com>
2026-03-03 20:51:30 +0100

    exclude vscode and license from sync

--- copy_to_server.sh
+++ copy_to_server.sh
@@ -3,7 +3,7 @@ SOURCE="/home/friedrich/Projects/Websites/friedrichosdev.duckdns.org/"
 DESTINATION="oracle-server:/var/www/html"
 
 # Delete old files on the server
-rsync -avz --exclude="copy_to_server.sh" --exclude=".git" --exclude=".gitignore" --delete "$SOURCE" "$DESTINATION"
+rsync -avz --exclude="copy_to_server.sh" --exclude=".git" --exclude=".gitignore" --exclude=".vscode" --exclude="LICENSE" --delete "$SOURCE" "$DESTINATION"
 
 # Sync the files
-rsync -avz --exclude="copy_to_server.sh" --exclude=".git" --exclude=".gitignore" "$SOURCE" "$DESTINATION"
+rsync -avz --exclude="copy_to_server.sh" --exclude=".git" --exclude=".gitignore" --exclude=".vscode" --exclude="LICENSE" "$SOURCE" "$DESTINATION"
